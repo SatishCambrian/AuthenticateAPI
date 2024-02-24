2a961ce5b808805f115bc0b1c87d184576048ef8
SatishCambrian <SatishCambrian@users.noreply.github.com>
2024-02-23 23:56:35 -0500

    fix error in shoppingcart test

--- AuthenticateApiTests/ShoppingCartTests.cs
+++ AuthenticateApiTests/ShoppingCartTests.cs
@@ -1,4 +1,6 @@
 using AuthenticateApiClassLibrary.Data;
+using AuthenticateWebAPI.Controllers;
+using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 namespace AuthenticateApiTests;
 
@@ -19,6 +21,20 @@ public class ShoppingCartTests
         Assert.IsTrue(properties.Contains(x => x.Name == "User"));
         Assert.IsTrue(properties.Contains(x => x.Name == "Products"));
     }
+[TestMethod]
+        public void RemoveProductFromShoppingCart_RemovesProductFromCart_ReturnsOk()
+        {
+            // Arrange
+            var productId = 1;
+            var controller = new ShoppingCartController(context);
 
-    
+            // Act
+            var result = controller.RemoveProductFromShoppingCart(productId);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var response = Assert.IsType<RemoveProductFromShoppingCartResponse>(okResult.Value);
+            Assert.Equal(productId, response.ProductId);
+            Assert.Equal("Product removed. Items in cart 1, now there is 0 items remaining", response.Message);
+        }
 }
